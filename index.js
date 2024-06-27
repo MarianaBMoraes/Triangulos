cf8d964c937f4ace003c9da6c9cc7ce17eba26c5
Mariana <marianabmoraes.ef@gmail.com>
2024-06-27 09:41:18 -0300

    feat: adiciona variaveis

---
+++ index.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+
+let lado1 = Number(prompt("Digite o comprimento do primeiro lado do triângulo: "));
+let lado2 = Number(prompt("Digite o comprimento do segundo lado do triângulo: "));
+let lado3 = Number(prompt("Digite o comprimento do terceiro lado do triângulo: "));
+
